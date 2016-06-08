25aaa8c88f76c4989c3ca6cc30821aa04a2af9f4
TestingDC <eseth3@yahoo.com>
2016-06-08 01:43:53 -0500

    Added some Structure.

--- Sandbox/src/Core/Game.java
+++ Sandbox/src/Core/Game.java
@@ -1,20 +1,49 @@
 package Core;
 
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.opengl.Display;
+
+
 public class Game {
 	
 	Window window = new Window();
 	
-	public static void init(String Args[]) {
+	
+	
+	public static void main(String Args[]) {
 		new Game();
 	}
 	
-	//---//
 	
 	public Game() {
-		System.out.println("### Init ###");
+		System.out.println("### Launching...");
+		loop();
+	}
+	
+	private void loop() {
+		while(!Display.isCloseRequested()) {
+			//if(Display.wasResized()) { }
+
+			glClear(GL_COLOR_BUFFER_BIT);
+			tick();
+			render();
+			
+			Display.update();
+			Display.sync(60);
+		}
+		Display.destroy();
+	}
+	
+	// ------------------------------------------------------------------------- //
+	
+	
+	private void tick() {
 		
 	}
 	
+	private void render() {
+		
+	}
 	
 	
 	
